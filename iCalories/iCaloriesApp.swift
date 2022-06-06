b80e084a844889b9ae6745095b91becaa2d67f86
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-06-06 09:59:39 +0200

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Cloud Secrets on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
